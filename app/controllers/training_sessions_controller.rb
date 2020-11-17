22013b9ba97f57bd7f02c9bb82a6e8c7480868f7
Sebastien Grima <sebastien.grima@gmail.com>
2020-11-17 17:05:29 +0000

    Added the form for a new session

--- app/controllers/training_sessions_controller.rb
+++ app/controllers/training_sessions_controller.rb
@@ -2,4 +2,28 @@ class TrainingSessionsController < ApplicationController
   def index
     @sessions = TrainingSession.all
   end
+
+  def new
+    @session = TrainingSession.new
+  end
+
+  def create
+   @session = TrainingSession.new(strong_params)
+   @session.trainer = current_user
+   if  @session.save!
+      redirect_to training_sessions_path(@session.id)
+    else
+      render :new
+    end
+  end
+
+  private 
+
+  def set_training_session
+    @training_session = TrainingSession.find(params[:id])
+  end
+
+  def strong_params
+    params.require(:training_session).permit(:category,:description, :cost, :start_time, :end_time)
+  end
 end
